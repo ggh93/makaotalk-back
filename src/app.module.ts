1415438f50854696aeaadda9d1e8929d32966742
ggh93 <ggh93@naver.com>
2025-12-24 23:11:27 +0900

    feat: board 폴더 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
+import { BoardService } from './Board/board.service';
+import { BoardController } from './Board/board.controllers';
+import { BoardModule } from './Board/board.module';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [BoardController, AppController],
+  providers: [BoardService, BoardModule, AppService],
 })
 export class AppModule {}
